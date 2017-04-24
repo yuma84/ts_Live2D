3ebae03cf0c4715bd8f30a041c8c25cd4f087f99
Yuma Hashimoto <yuma_hashimoto@c.dwango.co.jp>
2017-04-24 20:40:04 +0900

    fixed type definition

--- src/ts/Simple.d.ts
+++ src/ts/Simple.d.ts
@@ -1,23 +1,15 @@
-// declare class UtSystem {
-//     getUserTimeMSec (): number;
-// }
-//
-// declare class Live2D {
-//     init (): void;
-//     setGL ( gl: any, num?: number ): void;
-// }
-//
-// declare class Live2DModelWebGL {
-//     loadModel ( data: any ): void;
-// }
+declare namespace UtSystem {
+    function getUserTimeMSec (): number;
+}
 
-// export declare function UtSystem (): void;
-// export declare function Live2D (): void;
-// export declare function Live2DModelWebGL (): void;
+declare namespace Live2D {
+    function init (): void;
+    function setGL ( gl: any, num?: number ): void;
+}
 
-interface UtSystem {}
-interface Live2D {}
-interface Live2DModelWebGL {}
+declare namespace Live2DModelWebGL {
+    function loadModel ( data: any ): void;
+}
 
 interface Window {
     mozCancelAnimationFrame: any;
